b948340dee50153c05d6a6564f9598d21ae7b948
Svinci131 <svinci131@gmail.com>
2016-04-12 12:13:55 -0400

    first commit

---
+++ haiku.js
@@ -0,0 +1,37 @@
+var fs = require ("fs"),
+ 	haiku = require('./haiku'),
+	createHaiku = haiku.createHaiku
+// console.log(createHaiku());
+	cmudictFile = readCmudictFile('./cmudict.txt');
+
+function readCmudictFile(file){
+  return fs.readFileSync(file).toString();
+}
+
+
+function formatData(data){   
+   var words = {}; 
+   var lines = data.toString().split("\n"),
+       lineSplit
+   	lines.forEach(function(line){ 
+	    lineSplit = line.split("  "); 
+	    if (lineSplit[1]) {
+		    var word = lineSplit[0]; 
+		    var phonemes = lineSplit[1]; 
+		    var syllableCnt = (lineSplit[1].match(/[0-9]/g) || []).length;	    
+	    	if (!words[syllableCnt]){
+	    		words[syllableCnt] = []; 
+	    	}
+	    	words[syllableCnt].push(lineSplit[0])
+	    }
+	    
+	   	//console.log("The word " + lineSplit[0] + " phoneme layout: " + lineSplit[1], syllableCnt);  
+  	});   
+  	return words;
+}
+
+
+console.log(formatData(cmudictFile));
+
+
+
---
+++ haiku_generator.js
@@ -0,0 +1,11 @@
+
+function createHaiku(structure){
+	console.log(structure)
+    //console.log("this should log a haiku with the structure " + structure);
+}
+
+module.exports = {
+  createHaiku: createHaiku,
+};
+
+console.log(module);
