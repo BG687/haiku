eac061abce12ed095076bbd5e00880906aa5154d
Svinci131 <svinci131@gmail.com>
2016-04-13 13:07:16 -0400

    reorganized folders

---
+++ extraCredit/dictionary.js
@@ -0,0 +1,48 @@
+var fs = require ("fs"),
+ 	haiku = require('../haiku_generator'),
+	createHaiku = haiku.createHaiku
+	cmudictFile = readCmudictFile('../cmudict.txt');
+	wordObj = formatData(cmudictFile); 
+
+function readCmudictFile(file){
+  return fs.readFileSync(file).toString();
+}
+
+function formatData(data){   
+   var words = {}; 
+   var lines = data.toString().split("\n"),
+       lineSplit
+   	lines.forEach(function(line){ 
+	    lineSplit = line.split("  "); 
+	    if (lineSplit[1]) {
+		    var word = lineSplit[0]; 
+		    var phonemes = lineSplit[1]; 
+		    var syllableCnt = (lineSplit[1].match(/[0-9]/g) || []).length;	    
+	    	if (!words[syllableCnt]){
+	    		words[syllableCnt] = []; 
+	    	}
+	    	words[syllableCnt].push(lineSplit[0])
+	    }
+	});   
+  	return words;
+}
+
+function createDictionary (data) {   
+   var dictionary = {}; 
+   var lines = data.toString().split("\n"),
+       lineSplit
+   	lines.forEach(function(line){ 
+	    lineSplit = line.split("  "); 
+	    if (lineSplit[1]) {
+		    var word = lineSplit[0]; 
+		    var phonemes = lineSplit[1]; 
+		    var syllableCnt = (lineSplit[1].match(/[0-9]/g) || []).length;	    
+	    	dictionary[word] = {syllableCnt: syllableCnt}
+	    }
+	});   
+  	return dictionary;
+}
+
+module.exports = createDictionary(cmudictFile);
+
+//createHaiku([5,7,5], wordObj);
