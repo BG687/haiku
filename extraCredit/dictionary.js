26f313cb9ca586a465d7427021f4bf314998d594
Svinci131 <svinci131@gmail.com>
2016-04-13 13:17:32 -0400

    I think it's easier to edit when there are two files

--- extraCredit/dictionary.js
+++ extraCredit/dictionary.js
@@ -1,30 +1,8 @@
 var fs = require ("fs"),
- 	haiku = require('../haiku_generator'),
-	createHaiku = haiku.createHaiku
 	cmudictFile = readCmudictFile('../cmudict.txt');
-	wordObj = formatData(cmudictFile); 
 
 function readCmudictFile(file){
   return fs.readFileSync(file).toString();
-}
-
-function formatData(data){   
-   var words = {}; 
-   var lines = data.toString().split("\n"),
-       lineSplit
-   	lines.forEach(function(line){ 
-	    lineSplit = line.split("  "); 
-	    if (lineSplit[1]) {
-		    var word = lineSplit[0]; 
-		    var phonemes = lineSplit[1]; 
-		    var syllableCnt = (lineSplit[1].match(/[0-9]/g) || []).length;	    
-	    	if (!words[syllableCnt]){
-	    		words[syllableCnt] = []; 
-	    	}
-	    	words[syllableCnt].push(lineSplit[0])
-	    }
-	});   
-  	return words;
 }
 
 function createDictionary (data) {   
@@ -44,5 +22,3 @@ function createDictionary (data) {
 }
 
 module.exports = createDictionary(cmudictFile);
-
-//createHaiku([5,7,5], wordObj);
--- extraCredit/extraCredit2.js
+++ extraCredit/extraCredit2.js
@@ -1,11 +1,11 @@
 var fs = require ("fs"),
- 	haiku = require('../haiku_generator'),
+ 	haiku = require('./createHaiku'),
 	file = readCmudictFile('../timeMachine.txt'),
-	dictionary = require('./dictionary.js')
+	dictionary = require('./dictionary.js'),//
 	http = require("http"),
 	parseString = require('xml2js').parseString,
 	text = formatData(file),
-	skip = ["Project","and", "Gutenberg-tm"],
+	skip = ["Project","and", "Gutenberg-tm"],//
 	cantEndOn = ["I","was","had","my", "a","of","to", "at","the","with","soon"];
 
 function readCmudictFile(file){
@@ -26,106 +26,7 @@ function formatData(data){
 	return text;
 }
 
-function getRandomParagraph () {
-	var randomIndex = getRandom (Object.keys(text));
-	var randomPar = text[randomIndex].split(" ");
-	//get rid of quotes at beggining of paragraph
-	randomPar[0]=randomPar[0].replace(/'/g,"")
-	//pick another if that 
-	var firstChar = randomPar[0].split("")[0];
-	if (!isNaN(firstChar)) {
-		return getRandomParagraph();
-	}
-	else {
-		return randomPar
-	}
-}
-
-function createHaiku (obj) {
-	var order = [5,7,5]; 
-	var par;
-	for (i in order){
-		par = getRandomParagraph (obj); 
-		writeLine(par, order[i], 0);
-	}
-}
-
-function writeLine (par, length, start) {
-	var word = par[start];
-	//if paragraph runs out and word is undefined
-	//start at the beggining of the paragraph
-	if (!word) { 
-		console.log("TEST", word)
-		start = 0; 
-		word = par[start];
-		console.log("FIXED", word)
-	}
-	var sylNum = getSyllables(word);
-
-	if (length === 0) {
-		return ""
-	}
-	//check if it's in dictionary file
-	if (dictionary[word.toUpperCase()]){
-		sylNum = dictionary[word.toUpperCase()].syllableCnt 
-	}
-	//if too many syllables
-	if (sylNum > length) {
-		return writeLine(par, length, start+1)
-	}
-	//skip certain words always
-	if (skip.indexOf(word) >= 0) {
-		return writeLine(par, length, start+1)
-	}
-	//don't end with an adjective or preposition
-	if (cantEndOn.indexOf(word) >= 0 && length === 1) {
-		return writeLine(par, length, start+1)
-		//console.log("nope", word, length)
-	}
 
 
-	console.log("found", word, length)
-	return word+" "+writeLine(par, length-sylNum, start+1)
-}
 
-////create a rule for dipthongs client, riot, quiet, facial,
-function getSyllables (word) {
-	//get all vowels that aren't the last letter and aren't next to another vowel
-	var syllables = []; 
-	var holder = "";
-	for (var i = 0; i <word.length; i++){
-		holder+=word[i]
-		//your, he, quiet, boy, lynx, happy, preserve, resevior 
-		var isVowel = word[i].match(/[a,e,i,o,u,y]/g);
-		if (isVowel){
-			//if it's not the last one
-			if (word[i+1]){
-				//if the next one isn't a vowel
-				if (!word[i+1].match(/[a,e,i,o,u,y]/g)){ 
-					syllables.push(holder+"-")
-					holder = "";
-				}
-			}
-			else {
-			//feet
-				if (isVowel[0] !== "e") {
-					syllables.push(holder+"-")
-					holder = "";
-				}
-			}
-			
-		}
-		
-	}
-	//the, he
-	if (syllables.length === 0){
-		syllables.push(holder)
-	}
-	return syllables.length
-}
-
-function getRandom (arr) {
-	return arr[Math.floor(Math.random() * arr.length)];
-}
-
-createHaiku (text)
+haiku.createHaiku(text)
